2c1c1ef0e615cc88e203f686c1a6b710b9e91690
nginx4ops <datvu.1407@gmail.com>
2016-04-19 14:38:11 +0700

    fixxxxxx

--- index-en.php
+++ index-en.php
@@ -45,7 +45,7 @@ div.bd {width: 35em;margin: 0 auto;font-family: Tahoma, Verdana, Arial, sans-ser
 <p > Hello. Thank you for using my bash shell automatically installed Nginx 1.9.X and PHP 7.0</p> 
 <br />nginx configuration file  app-root/data/conf，PHP 7 configuration file  app-root/data/etc, upload code to  app-root/runtime/repo/www，<br />
 Nginx update: cd /tmp && wget http://dl.vuvdat.net/nginx-update-en && chmod +x nginx-update-en &&./nginx-update-en <br /> 
- PHP 7.0 update: cd /tmp && http://dl.vuvdat.net/php-update-en && chmod +x php-update-en && ./php-update-en  <br >wordpress install: wget http://dl.vuvdat.net/wp-en chmod +x wp-en && ./wp-en
+ PHP 7.0 update: cd /tmp && http://dl.vuvdat.net/php-update-en && chmod +x php-update-en && ./php-update-en  <br >wordpress install: wget http://dl.vuvdat.net/wp-en && chmod +x wp-en && ./wp-en
 <p>cd /tmp && wget --no-check-certificate http://dl.vuvdat.net/install-en && chmod +x install-en && ./install-en<br/>  </p>
 <a href="http://vuvdat.net/">My website</a> | contact me:<a href="mailto:help@vuvdat.net"></a>help@vuvdat.net  | <p class="flip1">server monitor</p> | <p class="flip" >phpinfo</p>
 </div>
